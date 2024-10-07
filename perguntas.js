5be1f97b8ee5d6a7bfb3cad4b67ea650114b2d22
zirrilo <163433495+zirrilo@users.noreply.github.com>
2024-10-07 08:04:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Lexus',
+    'Quanto custa um Lexus LFA no Brasil?',
+    '| 🚘 2012 Lexus LFA 🇧🇷 Preço convertido : 3.5 milhões de reais'
+)
+
+criaCartao(
+    'LFA',
+    'Quem fez o motor do Lexus LFA?',
+    'Modelo tem motor de 560 cavalos e foi criado do zero pela marca. '
+)
+
+criaCartao(
+    'LFA',
+    'Quem é dono da Lexus??',
+    'A Toyota Motor Corporation produz veículos sob cinco marcas: Toyota, Hino, Lexus, Ranz e Daihatsu.'
+)
+
+criaCartao(
+    'LeXUS LFA',
+    'O que significa LFA??',
+    'Legacy Fighting Alliance (LFA) fecha setembro de grandes eventos esportivos no Geraldão.'
+)
